f6e74d9a2b64a6dfbc6521b15a80be18f35bdfda
genisgio <elisabetta.casciano@student.univaq.it>
2024-05-17 22:36:01 +0200

    Creazione classe ECreditCard

---
+++ model/ECreditcard.php
@@ -0,0 +1,206 @@
+<?php
+
+/**
+ * La classe ECreditcard contiene tutte le proprietà (attributi e metodi) riguardanti una carta di credito
+ * Gli attributi o la descrivono sono:
+ * -card_id: è un identificativo autoincrement, relativo alla carta di credito;
+ * -card_holder: intestatario carta;
+ * -card_number: numero della carta;
+ * -security_code: codice di sicurezza a tre cifre;
+ * -expiration_date: data di scadenza della carta;
+ * -agenzia di appoggio :(es. poste italiane, bper, intesa sanpaolo, ecc...);
+ * -balance: saldo presente sulla carta
+ * 
+ */
+class ECreditcard{
+    /**
+     * identificativo univoco della carta di credito
+     * @AttributeType int
+     */
+    private $card_id;
+
+    /**
+     * intestatario carta
+     * @AttributeType string
+     */
+    private $card_holder;
+    
+
+    /**
+     * numero della carta
+     * @AttributeType String
+     */
+    private $card_number;
+
+    /**
+     * codice di sicurezza della carta
+     * @AttributeType string
+     */
+    private $security_code;
+
+    /**
+     * data di scadenza della carta nel formato mm/aa
+     * @AttributeType String
+     */
+    private $expiration_date;
+
+    /**
+     * agenzia di appoggio
+     * @AttributeType String
+     */
+    private $support_agency;
+
+    /**
+     * saldo presente sulla carta
+     * @AttributeType int
+     */
+    private $balance;
+
+    // CONSTRUCTOR
+    public function __construct($a,$b,$c,$d,$e) {
+        $this->card_holder = $a;
+        $this->card_number = $b;
+        $this->security_code=$c;
+        $this->expiration_date=$d;
+        $this->support_agency=$e;
+
+    }
+    // GET METHODS
+    /**
+     * Get the value of card_id
+     *
+     * @return $card_id
+     */
+
+    public function getCard_id() {
+		return $this->card_id;}
+
+    /**
+     * Get the value of card_holder
+     *
+     * @return $card_holder
+     */
+
+    public function getCreditCardHolder() {
+		return $this->card_holder;}
+
+    
+    /**
+     * Get the value of card_number
+     *
+     * @return $card_number
+     */
+    public function getCreditCardNumber() {
+		return $this->card_number;}
+
+
+    /**
+     * Get the value of security_code
+     *
+     * @return $security_code
+     */
+    public function getCreditCardSecurityCode() {
+        return $this->security_code;}
+
+
+    /**
+     * Get the value of expiration_date
+     * 
+     * @return $expiration_date
+     */
+    
+    public function getCreditCardExpirationDate() {
+        return $this->expiration_date;}
+
+
+    /**
+     * Get the value of support_agency
+     *
+     * @return $support_agency
+     */
+    
+    public function getCreditCardSupportAgency() {
+        return $this->support_agency;}
+    
+   /**
+     * Get the value of balance
+     *
+     * @return $balance
+     */
+    
+     public function getCreditCardBalance() {
+        return $this->balance;}
+    
+
+
+    // SET METHODS
+
+     /**
+     * Set the value of card_id
+     *
+     * @param $card_id
+     */
+
+    public function setCreditCard_id($card_id){
+        $this->card_id=$card_id;
+    }
+
+     /**
+     * Set the value of card_holder
+     *
+     * @param $card_holder
+     */
+    public function setCreditCardHolder($card_holder){
+        $this->card_holder=$card_holder;
+    }
+
+    
+    /**
+     * Set the value of card_number
+     * 
+     * @return $card_number
+     */
+    private function setCreditCardNumber($card_number) {
+        $this->card_number= $card_number;
+    }
+
+     /**
+     * Set the value of security_code
+     *
+     * @param $security_code
+     */
+    public function setCreditCardSecurityCode($security_code){
+        $this->security_code=$security_code;
+    }
+
+     /**
+     * Set the value of expiration_date
+     *
+     * @param $expiration_date
+     */
+
+    public function setCreditCardExpirationDate($expiration_date){
+        $this->expiration_date=$expiration_date;
+    }
+    
+     /**
+     * Set the value of support_agency
+     *
+     * @param $support_agency
+     */
+    public function setCreditCardSupportAgency($support_agency){
+        $this->support_agency=$support_agency;
+    }
+
+     /**
+     * Set the value of balance
+     *
+     * @param $balance
+     */
+    public function setBalance($balance){
+        $this->balance=$balance;
+    }
+
+
+}
+?>
